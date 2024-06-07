065849a83483370f8f2dced20a72b66b5c11c8fc
Tshine <gtshine@gmail.com>
2024-06-08 07:56:07 +0800

    shell

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
 fi
 
-python src/utils/front.py
+python3 src/utils/front.py
 
 nohup uvicorn src.main:app --port 7170 --host 0.0.0.0 --log-level warning > /dev/null 2>&1 &
 

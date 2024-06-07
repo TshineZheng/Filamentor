542030e8d9e9008acf5a4952d5c294ca32644622
Tshine <gtshine@gmail.com>
2024-06-08 07:55:10 +0800

    调整shell脚本

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
 fi
 
+python src/utils/front.py
+
 nohup uvicorn src.main:app --port 7170 --host 0.0.0.0 --log-level warning > /dev/null 2>&1 &
 
 echo "启动成功"

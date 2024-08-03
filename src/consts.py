95b2f66d0e5fcff151cad261b83c16eb72745518
Tshine <gtshine@gmail.com>
2024-08-03 15:11:00 +0800

    修复z轴抬高日志调整

--- src/consts.py
+++ src/consts.py
@@ -32,11 +32,11 @@ def setup():
 
     if FIX_Z and fix_z_gcode_path:
         from src.utils.log import LOGI, LOGE
-        LOGI('FIX_Z_TEMP:', FIX_Z_TEMP)
-        LOGI('FIX_Z_PAUSE_COUNT:', FIX_Z_PAUSE_COUNT)
-        LOGI('PAUSE_Z_OFFSET:', PAUSE_Z_OFFSET)
-        LOGI('DO_HOME_Z_HEIGHT:', DO_HOME_Z_HEIGHT)
-        LOGI('LAYER_HEIGHT:', LAYER_HEIGHT)
+        LOGI(f'FIX_Z_TEMP: {FIX_Z_TEMP}')
+        LOGI(f'FIX_Z_PAUSE_COUNT: {FIX_Z_PAUSE_COUNT}')
+        LOGI(f'PAUSE_Z_OFFSET: {PAUSE_Z_OFFSET}')
+        LOGI(f'DO_HOME_Z_HEIGHT: {DO_HOME_Z_HEIGHT}')
+        LOGI(f'LAYER_HEIGHT: {LAYER_HEIGHT}')
         LOGI(f'Z 轴抬高 gcode 文件路径为：{fix_z_gcode_path}')
         try:
             with open(fix_z_gcode_path, 'r') as f:
